d1cb7e4692c342c834d033e94a8fa337e148d6ad
GauravKhatarkar <89848947+GauravKhatarkar@users.noreply.github.com>
2021-08-31 18:01:50 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Armstrong{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int n = num;
+        int rem=0,sum=0;
+        while(n != 0){
+            rem = n % 10;
+            n = n/10;
+            sum += rem*rem*rem;
+        }
+        if(sum==num){
+            System.out.println("Number is Armstrong");
+        }else{
+            System.out.println("Number is not Armstrong");
+        }
+    }
+}
---
+++ Thirdhigh.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Thirdhigh{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array - ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter array - ");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int first,second,third;
+        first = second = third = arr[0];
+        
+        for(int i=1;i<n;i++){
+           
+           if(arr[i]>first){
+               third = second;
+               second = first;
+               first = arr[i];
+           }else if(arr[i]>second){
+               third = second;
+               second = arr[i];
+           }else if(arr[i]>third){
+               third = arr[i];
+           }
+            
+        }
+        System.out.println("third high is - "+third);
+    }
+}
